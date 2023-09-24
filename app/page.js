aa3d1a44ed89f5c53ab76a1230c5719ae08abf34
Nathuz <nathuznoepupin@gmail.com>
2023-09-24 19:46:20 +0200

    🎨 Home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+import Link from "next/link"
+
+export default function Home() {
+    return (
+      <section className="home">
+        <div className="home-pres">
+          <h1>WorLd</h1>
+          <h2 id="baseline">Words from the whole world</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur magna ligula, tincidunt nec tellus eget, luctus lacinia ligula. In tincidunt justo nec vestibulum molestie. Etiam libero risus, interdum eget lorem id, mollis facilisis eros. Praesent rutrum tortor a elit vulputate dictum. Nam justo lorem, pharetra eget luctus dictum, pharetra quis nibh. Cras auctor, metus vitae posuere auctor, nibh erat faucibus elit, eget cursus enim orci ut orci.</p>
+        </div>
+
+
+        <Link href="/chat" className="primary-btn">Acceder au chat</Link>
+      </section>
+    )
   }
