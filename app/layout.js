458f2805588505575a15cab021e4e821eb5e0877
Nathuz <nathuznoepupin@gmail.com>
2023-09-24 19:46:56 +0200

    ✏️  All typo

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,21 @@
 import main from './style/main.scss'
+import Navbar from './components/client/navbar'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 
 export default function RootLayout({ children }) {
     return (
-      <html lang="en">
-        <body>{children}</body>
+      <html lang="fr" className={roboto.className}>
+        <body>
+          <Navbar></Navbar>
+          {children}
+        </body>
       </html>
     )
   }
